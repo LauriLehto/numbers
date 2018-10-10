4180c222dd03937f73dc072dafc9006c1e089d5c
Jennie Dalgren <jennie.dalgren@gmail.com>
2018-10-10 22:54:10 +0200

    minor fixes

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { createStackNavigator } from "react-navigation";
 
-
 import RandomFact from "./src/RandomFact";
 import HomeScreen from "./src/HomeScreen";
 import FavNumber from "./src/FavNumber";
-import Quiz from './src/Quiz;
+import Quiz from "./src/Quiz";
 
 export default (App = createStackNavigator(
   {
